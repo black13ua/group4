f185b1dffa787946dddc1703983828ba94e57480
black <black@nb.124.local>
2018-03-01 20:45:28 +0200

    add p14 & p15

---
+++ p15.erl
@@ -0,0 +1,12 @@
+-module(p15).
+-export([replicate/2]).
+
+
+replicate(L, N) ->
+    replicate(L, N, 1, []).
+replicate([], _N, _C, Acc) ->
+    Acc;
+replicate([H|T], N, _C=N, Acc) ->
+    replicate(T, N, 1, [H|Acc]);
+replicate([H|T], N, C, Acc) ->
+    replicate([H|T], N, C+1, [H|Acc]).
