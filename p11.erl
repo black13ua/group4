f3df8a0615d3004ba44d632d40b9bdc141ebe1f0
black <black@deb.favorit>
2018-03-03 08:14:23 +0200

    homework #1 done

---
+++ p11.erl
@@ -0,0 +1,19 @@
+-module(p11).
+-export([encode_modified/1]).
+
+
+encode_modified(L) ->
+    encode_modified(L, 0, [], []).
+
+encode_modified([H|T], N, [H=_S|_AccT], Acc) ->
+    encode_modified(T, N+1, [H], Acc);
+encode_modified([H|T], 1, [S|_AccT], Acc) ->
+    encode_modified(T, 1, [H], [S|Acc]);
+encode_modified([H|T], N, [S|_AccT], Acc) ->
+    encode_modified(T, 1, [H], [{N,S}|Acc]);
+encode_modified([H|T], _N, [], Acc) ->
+    encode_modified(T, 1, [H], Acc);
+encode_modified([], 1, [S|_AccT], Acc) ->
+    p05:reverse([S|Acc]);
+encode_modified([], N, [S|_AccT], Acc) ->
+    p05:reverse([{N,S}|Acc]).
