f6f5ce2262f78342dd1fb9644a9c825b97aa7f1b
black <black@nb.124.local>
2018-03-09 15:06:14 +0200

    optimisation for flatten

--- homework1/p07.erl
+++ homework1/p07.erl
@@ -7,10 +7,8 @@ flatten(L) ->
 
 flatten([[]|T], Acc) ->
     flatten(T, Acc);
-flatten([[H1|[H2|T2]]|T], Acc) ->
-    flatten([H2,T2|T], [H1|Acc]);
 flatten([[H1|T1]|T], Acc) ->
-    flatten([T1|T], [H1|Acc]);
+    flatten([H1,T1|T], Acc);
 flatten([H|T], Acc) ->
     flatten(T, [H|Acc]);
 flatten([], Acc) ->
